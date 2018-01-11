49fbc063aad293420a6ae91efabd3888138685fc
Koh Wei Jie <contact@kohweijie.com>
2018-01-11 17:14:02 +0800

    Fixed typo and added --clear option

--- gen_wallet.py
+++ gen_wallet.py
@@ -1,20 +1,24 @@
 #!/usr/bin/env python
+import os
+import sys
 
 from moneropy import account
 
 seed, sk, vk, addr = account.gen_new_wallet()
 
-print "Copy the address, memonic seed & private keys exactly as shown. Store them safely."
+if len(sys.argv) > 1 and sys.argv[1] == "--clear":
+    os.system('clear')
+
+print "Copy the following text exactly as shown and store it offline:"
 print "Public Monero address:", addr
 print("NOTE: DO NOT SHARE THE INFO BELOW OR YOU WILL LOSE YOUR FUNDS:")
-print "Private memonic seed:"
+print "Private spend key:", sk
+print "Private view key: ", vk
+print "Private mnemonic seed:"
 i = 1
 for word in seed:
     print word,
     if i % 10 == 0:
         print
     i += 1
-
 print
-print "Private spend key:", sk
-print "Private view key:", vk
